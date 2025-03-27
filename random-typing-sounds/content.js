3279997633f3f396c003471358c3ae0ce73acb78
krnkiran22 <kirandev2210@gmail.com>
2025-03-27 12:01:13 +0530

    manifest

--- random-typing-sounds/content.js
+++ random-typing-sounds/content.js
@@ -9,16 +9,23 @@ chrome.storage.onChanged.addListener((changes) => {
   }
 });
 
-const soundFiles = [
-  chrome.runtime.getURL("sounds/bruh.mp3"),
-  chrome.runtime.getURL("sounds/applause.mp3"),
-  chrome.runtime.getURL("sounds/gameover.mp3")
-];
+// Map specific keys to specific sounds
+const keySoundMap = {
+  "1": chrome.runtime.getURL("sounds/applause.mp3"),
+  "2": chrome.runtime.getURL("sounds/bruh.mp3"),
+  "3": chrome.runtime.getURL("sounds/gameover.mp3"),
+  "4": chrome.runtime.getURL("sounds/sus.mp3"),
+  "5": chrome.runtime.getURL("sounds/whyareyougay.mp3"),
+  // You can map more keys if you want
+};
 
-document.addEventListener("keypress", () => {
+document.addEventListener("keydown", (e) => {
   if (isEnabled) {
-    const audio = new Audio(soundFiles[Math.floor(Math.random() * soundFiles.length)]);
-    audio.volume = 0.7;
-    audio.play();
+    const soundFile = keySoundMap[e.key.toLowerCase()];
+    if (soundFile) {
+      const audio = new Audio(soundFile);
+      audio.volume = 0.7;
+      audio.play();
+    }
   }
 });
